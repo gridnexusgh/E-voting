d1ca326cbb2f4e073d3840fb9ea4c0c62329e308
RockZ37 <delalirock5@gmail.com>
2026-07-03 21:32:24 +0000

    capture

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { ShieldCheck, Users, Building2, GraduationCap, FilePlus, Users as UsersIcon } from 'lucide-react';
-=======
-import { ShieldCheck, Users, Building2, GraduationCap, FilePlus, Users as UsersIcon, Settings, FileText } from 'lucide-react';
->>>>>>> 0be1c7e3522f3e55cac62c65274d602002fc8368
+import { ShieldCheck, Users, Building2, GraduationCap, FilePlus, Users as UsersIcon} from 'lucide-react';
 import { useAuth } from '../contexts/AuthContext';
 import { useEffect, useState } from 'react';
 import { supabase } from '../services/supabase';
